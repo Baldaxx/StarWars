e711b244e2cb7d99c2bd8109d613f14026cf0e97
virginie.bm13@icloud.com <t.tizio@mailbox.org>
2023-11-16 19:46:35 +0100

    mise a jour

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 
 "use strict";
 window.addEventListener("DOMContentLoaded", (event) => {
-  /* MENU */
-  const LeMenu = document.getElementById("LeMenu");
-  const CmdMenu = document.getElementById("CmdMenu");
-  CmdMenu.addEventListener('click',function(){
-    LeMenu.style.display = (LeMenu.style.display == 'none')? '':'none';
-  });
-  // au chargement de la page
-  window.onload = function(){
-    // on teste la largeur de la fenêtre
-    var ww = window.innerWidth; // en pixels
-    LeMenu.style.display = ( ww > 768 )? '':'none';
-    CmdMenu.style.display = ( ww > 768 )? 'none':'';
-  };
-  // au redimensionnement de la fenêtre
-  window.onresize = function(){
-    // on teste la largeur de la fenêtre
-    var ww = window.innerWidth; // en pixels
-    LeMenu.style.display = ( ww > 768 )? '':'none';
-    CmdMenu.style.display = ( ww > 768 )? 'none':'';
-  };
-
+    /* MENU */
+    const liste = document.getElementById("LeMenu");
+    const CmdMenu = document.getElementById("CmdMenu");
+    CmdMenu.addEventListener('click', function () {
+        liste.style.display = (liste.style.display == 'none') ? '' : 'none';
+    });
+    // au chargement de la page
+    window.onload = function () {
+        // on teste la largeur de la fenêtre
+        var ww = window.innerWidth; // en pixels
+        liste.style.display = (ww > 768) ? '' : 'none';
+        CmdMenu.style.display = (ww > 768) ? 'none' : '';
+    };
+    // au redimensionnement de la fenêtre
+    window.onresize = function () {
+        // on teste la largeur de la fenêtre
+        var ww = window.innerWidth; // en pixels
+        liste.style.display = (ww > 768) ? '' : 'none';
+        CmdMenu.style.display = (ww > 768) ? 'none' : '';
+    };
 });
