edc2e66f4bba94cc52028aa6f4ae03d42bc6e6d5
virginie.bm13@icloud.com <t.tizio@mailbox.org>
2023-11-17 10:51:47 +0100

    responsive avec Pierre

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 
-"use strict";
-window.addEventListener("DOMContentLoaded", (event) => {
-    /* MENU */
-    const liste = document.getElementById("LeMenu");
-    const CmdMenu = document.getElementById("CmdMenu");
-    CmdMenu.addEventListener('click', function () {
-        liste.style.display = (liste.style.display == 'none') ? '' : 'none';
-    });
-    // au chargement de la page
-    window.onload = function () {
-        // on teste la largeur de la fenêtre
-        var ww = window.innerWidth; // en pixels
-        liste.style.display = (ww > 768) ? '' : 'none';
-        CmdMenu.style.display = (ww > 768) ? 'none' : '';
-    };
-    // au redimensionnement de la fenêtre
-    window.onresize = function () {
-        // on teste la largeur de la fenêtre
-        var ww = window.innerWidth; // en pixels
-        liste.style.display = (ww > 768) ? '' : 'none';
-        CmdMenu.style.display = (ww > 768) ? 'none' : '';
-    };
+// "use strict";
+// window.addEventListener("DOMContentLoaded", (event) => {
+//     /* MENU */
+//     const liste = document.getElementById("LeMenu");
+//     const logo = document.getElementById("logo123");
+//     const CmdMenu = document.getElementById("CmdMenu");
+//     CmdMenu.addEventListener('click', function () {
+//         liste.style.display = (liste.style.display == 'none') ? '' : 'none';
+//         logo.style.display = (logo.style.display == 'none') ? '' : 'none';
+//     });
+//     // au chargement de la page
+//     window.onload = function () {
+//         // on teste la largeur de la fenêtre quand la page est entierement chargé initailement
+//         var ww = window.innerWidth; // en pixels
+//         liste.style.display = (ww > 768) ? '' : 'none';
+//         CmdMenu.style.display = (ww > 768) ? 'none' : '';
+//     };
+//     // au redimensionnement de la fenêtre a chaque redimensionnement de la page
+//     window.onresize = function () {
+//         // on teste la largeur de la fenêtre
+//         var ww = window.innerWidth; // en pixels
+//         liste.style.display = (ww > 768) ? '' : 'none';
+//         CmdMenu.style.display = (ww > 768) ? 'none' : '';
+//     };
+// });
+const myButton = document.querySelector('#CmdMenu');
+const myMenu = document.querySelector("#LeMenu");
+
+myButton.addEventListener('click', () => {
+    if(myMenu.style.display === "none"){
+        myMenu.style.display = "block";
+    }else {
+        myMenu.style.display = "none"
+    }
+    
 });
